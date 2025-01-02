3ca0211f7f0ad11f22163d1d1b896dd2a058a56a
synnefon <connorjhopkins@gmail.com>
2025-01-02 02:26:08 -0800

    making stuff prettier

--- app.py
+++ app.py
@@ -4,32 +4,41 @@ from shuffle_scorers import SCORE_TYPE
 from shuffle_simulator import shuffle_decks
 from shufflers import SHUFFLE_STRAT
 
+
 app = Flask(__name__)
 app.debug = True
+
+@app.route('/calculate', methods=['GET'])
+def calculate():
+    deck_size = request.args.get('deck_size', type=str)
+    selected_strat = request.args.get('selected_strat', type=str)
+    selected_scorer = request.args.get('selected_scorer', type=str)
+    selected_max_shuffles = request.args.get('selected_max_shuffles', type=str)
+    min_num_piles = request.args.get('min_num_piles', type=str)
+    max_num_piles = request.args.get('max_num_piles', type=str)
+
+    result = shuffle_decks(
+      SHUFFLE_STRAT[selected_strat], 
+      SCORE_TYPE[selected_scorer],
+      int(selected_max_shuffles),
+      int(deck_size),
+      int(min_num_piles),
+      int(max_num_piles)
+    )
+
+    return jsonify({'result': result.permutation})
 
 @app.route("/" , methods=['GET', 'POST'])
 def shuffle_simulation():
     max_shuffles = [str(i+1) for i in range(6)]
-    # max_shuffles.sort(reverse=True)
+    min_piles = [str(i+1) for i in range(9)]
 
     return render_template('simulation.html',
         strats=SHUFFLE_STRAT,
         scorers=SCORE_TYPE,
         max_shuffles=max_shuffles,
+        min_piles=min_piles
     )
-
-@app.route('/calculate', methods=['GET'])
-def calculate():
-    selected_strat = request.args.get('selected_strat', type=str)
-    selected_scorer = request.args.get('selected_scorer', type=str)
-    selected_max_shuffles = request.args.get('selected_max_shuffles', type=str)
-    result = shuffle_decks(
-      SHUFFLE_STRAT[selected_strat], 
-      SCORE_TYPE[selected_scorer],
-      int(selected_max_shuffles)
-    )
-
-    return jsonify({'result': result.permutation})
 
 if __name__ == "__main__":
     app.run()
--- shuffle_simulator.py
+++ shuffle_simulator.py
@@ -1,28 +1,24 @@
-from shuffle_scorers import SCORE_TYPE
-from shufflers import SHUFFLE_FUNCTION_MAP, SHUFFLE_STRAT, ShuffledDeck, cut_deck
+from shufflers import SHUFFLE_FUNCTION_MAP, ShuffledDeck, cut_deck
 
-MIN_PILES = 1
-MAX_PILES = 10
-DECK_SIZE = 100
-MAX_SHUFFLES = 6
-CUT_DECK = False
-
-SHUFFLE_STRATEGY = SHUFFLE_STRAT.PILE
-SCORER = SCORE_TYPE.SHANNON_ENTROPY
-
-
-
-def shuffle_decks(shuffle_strat, score_type, max_shuffles=MAX_SHUFFLES, should_cut_deck=False):
-    PILE_DIVISIONS = [i for i in range(MIN_PILES, MAX_PILES+1)]
-    BASE_CARD_LIST = [i for i in range(DECK_SIZE)]
+def shuffle_decks(
+        shuffle_strat,
+        score_type,
+        max_shuffles,
+        deck_size,
+        min_num_piles,
+        max_num_piles,
+        should_cut_deck=False
+    ):
+    
+    PILE_DIVISIONS = [i for i in range(min_num_piles, max_num_piles+1)]
+    BASE_CARD_LIST = [i for i in range(deck_size)]
     shuffle_function = SHUFFLE_FUNCTION_MAP[shuffle_strat] if not should_cut_deck \
         else cut_deck(SHUFFLE_FUNCTION_MAP[shuffle_strat](deck, num_piles))
 
     shuffled_decks = [ShuffledDeck(BASE_CARD_LIST, (), score_type)]
     best = None
 
-    for i in range(max_shuffles):
-        print(i+1, "ROUNDS")
+    for _ in range(max_shuffles):
         new_decks = []
         for deck in shuffled_decks:
             for num_piles in PILE_DIVISIONS:
@@ -32,12 +28,6 @@ def shuffle_decks(shuffle_strat, score_type, max_shuffles=MAX_SHUFFLES, should_c
         shuffled_decks.sort(key=lambda deck: deck.score, reverse=True)
 
         for deck in shuffled_decks[0: 5]:
-            deck.print()
             best = shuffled_decks[0]
 
-        print()
-    print(shuffled_decks)
     return best
-    
-
-# shuffle_decks(SHUFFLE_STRATEGY, SCORER, CUT_DECK)
